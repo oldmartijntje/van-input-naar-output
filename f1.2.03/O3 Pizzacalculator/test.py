7442addbbcf7011ee9bdae9518fccdc3e934546a
Martijn Van Houwelingen <oldmartijntje@gmail.com>
2021-09-09 18:51:37 +0200

    yeeehooo, finally done

---
+++ f1.2.03/O3 Pizzacalculator/test.py
@@ -0,0 +1,4 @@
+import test1
+# lots of stuff here
+input("e")
+test1.main() # do whatever is in test1.py
---
+++ f1.2.03/O3 Pizzacalculator/test1.py
@@ -0,0 +1,6 @@
+def main():
+    print("I am a test")
+    print("see! I do nothing productive.")
+    input("e")
+if __name__ == "__main__":
+    main()
