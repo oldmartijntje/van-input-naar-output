f26e2f8a2827f5781e7bc69204806057af764941
Martijn Van Houwelingen <oldmartijntje@gmail.com>
2021-09-09 10:25:31 +0200

    i smell like cheese

---
+++ f1.2.03/L1 Invoer van NAW/person.py
@@ -0,0 +1,12 @@
+naam = input("wat is uw naam? >>>")
+adres = input("wat is uw adres? >>>")
+postcode = input("wat is uw postcode? >>>")
+woonplaats = input("wat is uw woonplaats? >>>")
+
+
+print("----------------------------------------------------")
+print("|  Naam      : " + naam)
+print("|  Adres     : " + adres)
+print("|  Postcode  : " + postcode)   
+print("|  Woonplaats: " + woonplaats)  
+print("----------------------------------------------------")
